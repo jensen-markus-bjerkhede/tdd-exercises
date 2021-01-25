f23f22d7c3177116042ad19ab4f2698629541239
Markus <>
2021-01-25 13:20:49 +0100

    Add stubbed version of method store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,3 @@
-let savedValue;
-
 function store(value) {
-	if( value !== undefined )
-		savedValue = value;
-}
 
-function retrieve() {
-	return savedValue;
 }
-
-module.exports = { store, retrieve };
